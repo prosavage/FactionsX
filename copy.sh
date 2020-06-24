37d7b767a33d61b78fa25fcd79ea33b6e65420d0
ProSavage <naman0311@gmail.com>
2020-06-24 01:42:09 -0500

    chore: Ignore copy shell script.

---
+++ copy.sh
@@ -0,0 +1,6 @@
+cp ~/IdeaProjects/FactionsX-Parent/FactionsX/target/FactionsX-0.5-PATREON.jar ~/IdeaProjects/FactionsX-Parent/debug/plugins/FactionsX-1.0.jar
+cp ~/IdeaProjects/FactionsX-Parent/FGrace-Addon/target/FGrace-Addon-1.0.jar ~/IdeaProjects/FactionsX-Parent/debug/plugins/FactionsX/addons/
+cp ~/IdeaProjects/FactionsX-Parent/FPrinter-Addon/target/FPrinter-Addon-1.0.jar ~/IdeaProjects/FactionsX-Parent/debug/plugins/FactionsX/addons/
+cp ~/IdeaProjects/FactionsX-Parent/FTOP-Addon/target/FTOP-Addon-1.0.jar ~/IdeaProjects/FactionsX-Parent/debug/plugins/FactionsX/addons/
+cp ~/IdeaProjects/FactionsX-Parent/FTNT-Addon/target/FTNT-Addon-1.0.jar ~/IdeaProjects/FactionsX-Parent/debug/plugins/FactionsX/addons/
+cp ~/IdeaProjects/FactionsX-Parent/FWild-Addon/target/FWild-Addon-1.0.jar ~/IdeaProjects/FactionsX-Parent/debug/plugins/FactionsX/addons/
