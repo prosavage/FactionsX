3716eb9868d4ba9c5f3e7b1733e755d806b70550
Naman Gupta <naman0311@gmail.com>
2020-09-07 13:07:16 -0500

    Push refs

--- copy.sh
+++ copy.sh
@@ -1,6 +1,7 @@
-cp ~/IdeaProjects/FactionsX-Parent/FactionsX/target/FactionsX-0.5-PATREON.jar ~/IdeaProjects/FactionsX-Parent/debug/plugins/FactionsX-1.0.jar
-cp ~/IdeaProjects/FactionsX-Parent/FGrace-Addon/target/FGrace-Addon-1.0.jar ~/IdeaProjects/FactionsX-Parent/debug/plugins/FactionsX/addons/
-cp ~/IdeaProjects/FactionsX-Parent/FPrinter-Addon/target/FPrinter-Addon-1.0.jar ~/IdeaProjects/FactionsX-Parent/debug/plugins/FactionsX/addons/
-cp ~/IdeaProjects/FactionsX-Parent/FTOP-Addon/target/FTOP-Addon-1.0.jar ~/IdeaProjects/FactionsX-Parent/debug/plugins/FactionsX/addons/
-cp ~/IdeaProjects/FactionsX-Parent/FTNT-Addon/target/FTNT-Addon-1.0.jar ~/IdeaProjects/FactionsX-Parent/debug/plugins/FactionsX/addons/
-cp ~/IdeaProjects/FactionsX-Parent/FWild-Addon/target/FWild-Addon-1.0.jar ~/IdeaProjects/FactionsX-Parent/debug/plugins/FactionsX/addons/
+cp ~/IdeaProjects/FactionsX-Parent/FactionsX/target/FactionsX-1.0.3-RC.jar ~/Servers/serverfast/plugins/FactionsX-1.0.jar
+cp ~/IdeaProjects/FactionsX-Parent/FGrace-Addon/target/FGrace-Addon-1.0.3-RC.jar ~/Servers/serverfast/plugins/FactionsX/addons/FGrace.jar
+cp ~/IdeaProjects/FactionsX-Parent/FPrinter-Addon/target/FPrinter-Addon-1.0.3-RC.jar ~/Servers/serverfast/plugins/FactionsX/addons/FPrinter.jar
+cp ~/IdeaProjects/FactionsX-Parent/FTOP-Addon/target/FTOP-Addon-1.0.3-RC.jar ~/Servers/serverfast/plugins/FactionsX/addons/FTOP.jar
+cp ~/IdeaProjects/FactionsX-Parent/FTNT-Addon/target/FTNT-Addon-1.0.3-RC.jar ~/Servers/serverfast/plugins/FactionsX/addons/FTNT.jar
+cp ~/IdeaProjects/FactionsX-Parent/FWild-Addon/target/FWild-Addon-1.0.3-RC.jar ~/Servers/serverfast/plugins/FactionsX/addons/FWild.jar
+cp ~/IdeaProjects/FactionsX-Parent/FChest-Addon/target/FChest-Addon-1.0.3-RC.jar ~/Servers/serverfast/plugins/FactionsX/addons/FChest.jar
