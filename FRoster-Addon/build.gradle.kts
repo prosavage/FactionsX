dea92928d869b741fc4bcdb6f5c745bd72494227
Naman Gupta <naman0311@gmail.com>
2020-09-09 12:43:37 -0500

    Gradle port :P

---
+++ FRoster-Addon/build.gradle.kts
@@ -0,0 +1,34 @@
+import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
+
+plugins {
+    java
+    kotlin("jvm")
+    id("com.github.johnrengelman.shadow")
+}
+
+dependencies {
+    compileOnly(project(":FactionsX"))
+    compileOnly(project(":AddonFramework"))
+    compileOnly("net.prosavage:BasePlugin:1.7.6")
+    compileOnly(kotlin("stdlib-jdk8"))
+    compileOnly ("org.spigotmc:spigot-api:1.16.1-R0.1-SNAPSHOT")
+}
+
+
+tasks {
+
+    compileKotlin {
+        kotlinOptions { jvmTarget = "1.8" }
+        sourceCompatibility = "1.8"
+    }
+    val shadowJar = named<ShadowJar>("shadowJar") {
+        mergeServiceFiles()
+        minimize()
+        val shadePath = "net.prosavage.factionsx.shade"
+        relocate("kotlin", "$shadePath.kotlin")
+        relocate("com.cryptomorin.xseries", "$shadePath.xseries")
+        relocate("net.prosavage.factionsx.addonframework", "$shadePath.addonframework")
+        archiveFileName.set("FRoster-Addon.jar")
+        println("Compiled FRoster-Addon")
+    }
+}
