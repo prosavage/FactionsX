8746db7c5f5758c2e6219df6a6f9168ab97d322e
Oliwer <rarlabdev@gmail.com>
2021-04-26 19:11:13 +0200

    WorldGuard safezone and warzone destroyable blocks flag(s).

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ import org.gradle.kotlin.dsl.support.zipTo
 
 allprojects {
     group = "net.prosavage.factionsx"
-    version = "1.1.1-STABLE"
+    version = "1.2-STABLE"
 }
 
 plugins {
@@ -76,7 +76,8 @@ tasks {
                         "* FactionsUUIDAPIProxy is a minecraft plugin and goes in \"/plugins/\".\n" +
                         "\n" +
                         "\n" +
-                        "Basic documentation for permissions or commands specific to this build can be found in the \"/plugins/FactionsX/docs\" folder\n" +
+                        "Basic documentation for permissions or commands specific to this build can be found in the \"/plugins/FactionsX/docs\" " +
+                        "folder after the first time starting up\n" +
                         "\n" +
                         "Discord: https://discord.gg/savagelabs\n" +
                         "Wiki: https://wiki.savagelabs.net"
